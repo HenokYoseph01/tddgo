5bf4815b2aaaeebc892fdd28cc02c30c7fe96248
HenokYoseph01 <henok.y2014@gmail.com>
2026-06-24 10:20:27 +0300

    Refactor Hello function to accept language parameter and update tests accordingly

--- hello_test.go
+++ hello_test.go
@@ -6,7 +6,7 @@ func TestHello(t *testing.T) {
 
 	t.Run("saying hello to people", func(t *testing.T) {
 
-		got := Hello("Henok")
+		got := Hello("Henok", "")
 		want := "Hello, Henok!"
 
 		assertCorrectMessage(t, got, want)
@@ -14,11 +14,23 @@ func TestHello(t *testing.T) {
 	})
 
 	t.Run("say 'Hello, World!' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World!"
 
 		assertCorrectMessage(t, got, want)
 
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Henok", "Spanish")
+		want := "Hola, Henok!"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Henok", "French")
+		want := "Bonjour, Henok!"
+		assertCorrectMessage(t, got, want)
 	})
 }
 
